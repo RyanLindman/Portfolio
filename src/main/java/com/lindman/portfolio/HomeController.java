ecde91117d97494fe32c010d6b45b72f8df31a11
RyanLindman <ryan.lindman@outlook.com>
2023-09-27 20:26:21 +0200

    created new project to use for aws exam

---
+++ src/main/java/com/lindman/portfolio/HomeController.java
@@ -0,0 +1,28 @@
+package com.lindman.portfolio;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome(){
+
+        return "index";
+    }
+
+    @GetMapping("/projects")
+    public String getProjects(){
+
+        return "projects";
+    }
+    @GetMapping("/contact")
+    public String getContact() {
+
+        return "contact";
+    }
+}
+
+
